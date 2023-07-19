0d5f926a2aae855330dcb3c0c89e0f5e99f30f8b
Venkatasai-Thalapalli <venkata0523@gmail.com>
2023-07-19 11:55:29 +0530

    commit

---
+++ Day8Ex1/Game.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Day8Ex1
+{
+    public class Game
+    {
+        int el;
+        int wp;
+        int s;
+
+        public int EL { get { return el; }  }
+        public int WP { get { return wp; } }
+        public void InitialSpin()
+        {
+            el = 1;
+            wp = 100;
+            s = 5;   
+        }
+        public void FirstSpin()
+        {
+            el += 1;
+            wp += 10;    
+        }
+        public void SecondSpin()
+        {
+            el += 2;
+            wp += 20;
+        }
+        public void ThirdSpin()
+        {
+            el -= 3;
+            wp -= 30;
+        }
+        public void FourthSpin()
+        {
+            el += 4;
+            wp += 40;
+        }
+        public void FifthSpin()
+        {
+            el -= 5;
+            wp -= 50;
+        }
+        public void SixthSpin()
+        {
+            el -= 1;
+            wp -= 60;
+        }
+        public void SeventhSpin()
+        {
+            el += 1;
+            wp += 70;
+        }
+        public void EighthSpin()
+        {
+            el -= 2;
+            wp -= 20;
+        }
+        public void NinthSpin()
+        {
+            el -= 3;
+            wp -= 30;
+        }
+        public void TenthSpin()
+        {
+            el += 10;
+            wp += 100;
+        }
+    }
+}
---
+++ Day8Ex1/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Day8Ex1
+{
+    public delegate void SpinGame();
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Game g = new Game();
+            SpinGame spingame = new SpinGame(g.FirstSpin);
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter your lucky number between 1 to 10");
+                int Luckynumber = int.Parse(Console.ReadLine());
+                switch (Luckynumber)
+                {
+                    case 1:
+                        {
+                            spingame += g.FirstSpin;
+                            break;
+                        }
+                    case 2:
+                        {
+                            spingame += g.SecondSpin;
+                            break;
+                        }
+                    case 3:
+                        {
+                            spingame += g.ThirdSpin;
+                            break;
+                        }
+                    case 4:
+                        {
+                            spingame += g.FourthSpin;
+                            break;
+                        }
+                    case 5:
+                        {
+                            spingame += g.FifthSpin;
+                            break;
+                        }
+                    case 6:
+                        {
+                            spingame += g.SixthSpin;
+                            break;
+                        }
+                    case 7:
+                        {
+                            spingame += g.SeventhSpin;
+                            break;
+                        }
+                    case 8:
+                        {
+                            spingame += g.EighthSpin;
+                            break;
+                        }
+                    case 9:
+                        {
+                            spingame += g.NinthSpin;
+                            break;
+                        }
+                    case 10:
+                        {
+                            spingame += g.TenthSpin;
+                            break;
+
+                        }
+                    default:
+                        {
+                            Console.WriteLine("invalid number");
+                            return;
+                        }
+                }
+
+            }
+            Console.WriteLine("Result:");
+            spingame();
+            if(g.EL >= 4 && g.WP > 60)
+            {
+                Console.WriteLine("winner");
+            }
+            else if (g.EL>= 1 && g.WP > 50)
+            {
+                Console.WriteLine("runner up");
+            }
+            else
+            {
+                Console.WriteLine("loser");
+            }
+            Console.ReadKey();
+        }
+    }
+}
