f4836a984d516e5820ae87a855863c808f966d91
Venkatasai-Thalapalli <venkata0523@gmail.com>
2023-07-21 16:14:52 +0530

    commit

---
+++ Day10Ex1/Calculation/CalcTestClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculation
+{
+    [TestFixture]
+    public class CalcTestClass
+    {
+        Calc c = new Calc();
+        [Test]
+        public void AddTest()
+        {
+            Assert.AreEqual(12,c.Add(8, 4));
+        }
+        [Test]
+        public void SubTest()
+        {
+            Assert.AreEqual(4, c.Subtract(8, 4));
+        }
+        [Test]
+        public void MulTest()
+        {
+            Assert.AreEqual(32, c.Multiply(8, 4));
+        }
+        [Test]
+        public void DivTest()
+        {
+            Assert.AreEqual(2, c.Divide(8, 4));
+        }
+        [Test]
+        public void PrimeTest()
+        {
+            Assert.IsTrue(c.Prime(2));
+        }
+
+
+
+    }
+}
---
+++ Day10Ex1/Calculation/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    public class Calc
+    {
+        public int Add(int a , int b)
+        {
+            return a + b;
+        } 
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return (a * b);
+        }
+        public bool Prime(int num)
+        {
+            if (num <= 1) return false;
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
