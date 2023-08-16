fae4b96b6f54bc1349ff98d961488d6765b62c3d
Venkatasai-Thalapalli <venkata0523@gmail.com>
2023-08-16 15:39:19 +0530

    commit

---
+++ WebAppCourses/Data/CourseDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAppCourses.Data
+{
+    public class CourseDbContext : DbContext
+    {
+        public CourseDbContext(DbContextOptions<CourseDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<WebAppCourses.Models.Course> Courses { get; set; } = default!;
+    }
+}
---
+++ WebAppCourses/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppCourses.Models
+{
+    [Table("Course")]
+    public class Course
+    {
+        [Key]
+        public int CId { get; set; }
+        public string CName { get; set; }
+        public float CFee { get; set; }
+        public string Status { get; set; }
+        public string Technology { get; set; }
+
+    }
+    
+}
