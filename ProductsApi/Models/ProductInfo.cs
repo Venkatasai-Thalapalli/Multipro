5d33d04f46a5593c8e75c0427d3994a78697e39e
Venkatasai-Thalapalli <venkata0523@gmail.com>
2023-08-21 15:54:57 +0530

    Commit

---
+++ ProductsApi/Models/ProductInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductsApi.Models;
+
+public partial class ProductInfo
+{
+    public int Pid { get; set; }
+
+    public string Pname { get; set; } = null!;
+
+    public double? Pprice { get; set; }
+
+    public DateTime? Pmdate { get; set; }
+
+    public int? Cid { get; set; }
+}
